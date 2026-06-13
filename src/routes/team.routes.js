651d2422b91b345cc06a9bb199e99e1c447f02c2
Sky-787 <darodriguez23@itp.edu.co>
2026-06-13 09:29:12 -0500

    feat: add GET /teams endpoint for frontend catalogue

--- src/routes/team.routes.js
+++ src/routes/team.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { createTeam, addStudentToTeam, getTeamStudents } = require('../controllers/team.controller');
+const { createTeam, getAllTeams, addStudentToTeam, getTeamStudents } = require('../controllers/team.controller');
 
 const router = Router();
 
+router.get('/', getAllTeams);
 router.post('/', createTeam);
 router.post('/:id/students', addStudentToTeam);
 router.get('/:id/students', getTeamStudents);
